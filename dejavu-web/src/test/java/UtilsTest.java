2a9261d852adffe8d5f218e8bc6adc41e60079be
Macrylynn <530454062@qq.com>
2019-10-21 09:51:17 +0800

    初始化

---
+++ dejavu-web/src/test/java/UtilsTest.java
@@ -0,0 +1,13 @@
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+/**
+ * @ClassName UtilsTest
+ * @Description
+ * @Author: lijiao73
+ * @Date: 2019/10/21 9:35
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+public class UtilsTest {
+}
