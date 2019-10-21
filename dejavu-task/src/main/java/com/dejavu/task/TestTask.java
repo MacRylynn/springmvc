0be9ca321de5dae305b7dce3f95b1096d61d66e1
Macrylynn <530454062@qq.com>
2019-10-21 10:51:23 +0800

    初始化

---
+++ dejavu-task/src/main/java/com/dejavu/task/TestTask.java
@@ -0,0 +1,10 @@
+package com.dejavu.task;
+
+/**
+ * @ClassName TestTask
+ * @Description
+ * @Author: lijiao73
+ * @Date: 2019/10/21 10:51
+ */
+public class TestTask {
+}
--- dejavu-web/src/test/java/UtilsTest.java
+++ dejavu-web/src/test/java/UtilsTest.java
@@ -1,3 +1,4 @@
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -10,4 +11,9 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 public class UtilsTest {
+    @Test
+    public void lambdaTest() {
+
+
+    }
 }
